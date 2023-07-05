c44ef7f30792f4189bed08a8b8cea19fcf325fa2
Koded <sogoseriki@gmail.com>
2023-07-06 00:22:40 +0100

    initial commit

--- submit.php
+++ submit.php
@@ -1,15 +1,28 @@
-$name = $_POST['name'];
+<?php
+$servername = "server582"; 
+$username = "sogo";
+$password = "Sogo@123";
+$dbname = "records";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name']; 
 $email = $_POST['email'];
 
-// Assuming you have an established database connection ($conn)
-$sql = "INSERT INTO regrec (name, email) VALUES ('$name', '$email')";
+$sql = "INSERT INTO regrec (name, email) 
+VALUES ('$name', '$email')";
 
-if ($conn->query($sql) === true) {
-    // Data inserted successfully
-    echo "Thank you for subscribing!";
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
 } else {
-    // Error occurred
-    echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $sql . "<br>" . $conn->error; 
 }
 
 $conn->close();
+?>
