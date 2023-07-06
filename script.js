a6d6404727a15f70859a4977f908c00762dd93c5
Koded <sogoseriki@gmail.com>
2023-07-06 04:00:57 +0100

    initial commit

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ document.getElementById("emailForm").addEventListener("submit", function(event) 
     event.preventDefault(); // Prevent the form from submitting normally
   
     var form = event.target;
-    var formData = new FormData(form); // Get the form data
+    
+    // Add "hidden" class to the form
+    form.classList.add("hidden");
   
+    // Remove "hidden" class from the modal
+    let moda = document.getElementsByClassName("moda");
+    moda.classList.remove("hidden");
+    
     // Send a POST request to the server-side script
     fetch(form.action, {
       method: form.method,
-      body: formData
+      body: new FormData(form) 
     })
     .then(function(response) {
       return response.text(); // Extract the response body as text
@@ -17,16 +23,8 @@ document.getElementById("emailForm").addEventListener("submit", function(event) 
       document.getElementById("responseMessage").innerHTML = data;
       document.getElementById("responseMessage").style.display = "block";
       form.reset(); // Reset the form fields
-  
-      // Add "hidden" class to the form
-      form.classList.add("hidden");
-  
-      // Remove "hidden" class from the modal
-      let moda = document.getElementsByClassName("moda");
-      moda.classList.remove("hidden");
     })
     .catch(function(error) {
       console.log("Error:", error);
     });
   });
-  
